5086b861d208405f896f144c05b115301686a2b1
Bartosz Płóciennik <b.plociennik@merixstudio.com>
2025-11-04 21:35:58 +0100

    Add favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import type { LayoutData } from './$types';
-	import favicon from '$lib/assets/favicon.svg';
+	import favicon from '$lib/assets/favicon.png';
 	import Navbar from '$lib/components/Navbar.svelte';
 	import GlobalLoader from '$lib/components/GlobalLoader.svelte';
 	import ToastContainer from '$lib/components/ToastContainer.svelte';
